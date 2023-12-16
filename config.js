f391d517a374f06b66399c3935d786781393afdf
Nihaal1234 <117754242+Nihaal1234@users.noreply.github.com>
2023-12-16 14:15:53 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCewkKJnmE4FHjV69cjyvjej04Rres1w5U",
+    authDomain: "team-voting-app-7a1e0.firebaseapp.com",
+    projectId: "team-voting-app-7a1e0",
+    storageBucket: "team-voting-app-7a1e0.appspot.com",
+    messagingSenderId: "124286861053",
+    appId: "1:124286861053:web:504ec9baf0328dfbe74894"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
